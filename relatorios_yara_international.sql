796c2f0003fb9c9b05fcc8528c55e0be32da978b
Dennis Cardoso <dennis.cardoso@outlook.com>
2018-09-02 10:26:41 -0300

    Update
    Atualização Documentos

---
+++ relatorios_yara_international.sql
@@ -0,0 +1,18 @@
+select * from lavoura;
+
+#Insert Line - Lavoura
+Insert into Lavoura (Id_fazenda, Id_tipo_cultivo, Nome_talhao, lat_talhao, long_talhao, espacamento_lavoura, populacao_lavoura, Cultura_ante_lavoura, MeioCultivo_lavoura, TamColhido_lavoura)
+				  values (1, 1, 'Talhao teste 1', 41.40, 2.17, 0, 0, 'Soja', 'meio de cultivo exemplo 1', 15.00);
+Insert into Lavoura (Id_fazenda, Id_tipo_cultivo, Nome_talhao, lat_talhao, long_talhao, espacamento_lavoura, populacao_lavoura, Cultura_ante_lavoura, MeioCultivo_lavoura, TamColhido_lavoura)
+				  values (2, 2, 'Talhao teste 2', 41.40, 2.17, 22.02, 30.10, 'Soja', 'Soja', 15.00);
+Insert into Lavoura (Id_fazenda, Id_tipo_cultivo, Nome_talhao, lat_talhao, long_talhao, espacamento_lavoura, populacao_lavoura, Cultura_ante_lavoura, MeioCultivo_lavoura, TamColhido_lavoura)
+				  values (3, 2, 'Talhao teste 2', 41.40, 2.17, 22.02, 30.10, 'Soja', 'Soja', 20.00);
+Insert into Lavoura (Id_fazenda, Id_tipo_cultivo, Nome_talhao, lat_talhao, long_talhao, espacamento_lavoura, populacao_lavoura, Cultura_ante_lavoura, MeioCultivo_lavoura, TamColhido_lavoura)
+				  values (4, 2, 'Talhao teste 2', 41.40, 2.17, 22.02, 30.10, 'Soja', 'Milho', 150.00);
+Insert into Lavoura (Id_fazenda, Id_tipo_cultivo, Nome_talhao, lat_talhao, long_talhao, espacamento_lavoura, populacao_lavoura, Cultura_ante_lavoura, MeioCultivo_lavoura, TamColhido_lavoura)
+				  values (5, 2, 'Talhao teste 2', 41.40, 2.17, 22.02, 30.10, 'Soja', 'Milho', 900.00);
+Insert into Lavoura (Id_fazenda, Id_tipo_cultivo, Nome_talhao, lat_talhao, long_talhao, espacamento_lavoura, populacao_lavoura, Cultura_ante_lavoura, MeioCultivo_lavoura, TamColhido_lavoura)
+				  values (5, 2, 'Talhao teste 2', 41.40, 2.17, 22.02, 30.10, 'Soja', 'Milho', 10000.00);
+                  
+                  
+delete from lavoura 
