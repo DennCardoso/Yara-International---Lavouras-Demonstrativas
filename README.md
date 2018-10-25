198a908025acb99072bd79e6d27a419aae443ae9
Dennis Cardoso <dennis.cardoso@outlook.com>
2018-10-25 19:08:28 -0300

    This is my first commit

--- README.md
+++ README.md
@@ -21,4 +21,5 @@ Aplicações utilizadas:
 #Visual Paradigm
 #MySQL  Workbench
 #Sublime Text
+#
 
---
+++ arquivoteste.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np
+
+dados =  pd.read_csv('/home/dennis/IMPRIMIR_Região_Geográfica.csv')
+
+print dados
